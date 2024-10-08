c4f94011c8646855e44c87746b88d29b81c9f6c5
Matius ewani <ewanimatex@gmail.com>
2024-10-08 07:55:10 -0700

    sacode-laravel-2024-pertemuan-17-matius

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+
+class Mobil
+{
+
+    //Property 
+    public $merek;
+    public $model;
+    public $tahun;
+    public $warna;
+
+    //Metode
+    public function start()
+    {
+        echo 'Mobil dinyalakan';
+    }
+
+    public function cek($nama, $ket)
+    {
+        echo 'Merek : ' . $this->merek . '<br> ';
+        echo 'Model : ' . $this->model . '<br> ';
+        echo 'Tahun : ' . $this->tahun . '<br> ';
+        echo 'Warna : ' . $this->warna . '<br> ';
+        echo 'Pemilik : ' . $nama . '<br>';
+        echo 'Keterangan  : ' . $ket . '<br>';
+        echo '<br>';
+    }
+
+    public function stop()
+    {
+        echo 'Mobil dimatikan';
+    }
+}
+
+class Kedaraan extends Mobil
+{
+
+    public $jenis;
+
+    public function bergerak()
+    {
+        echo 'kendaraan sedang bergerak';
+    }
+}
+
+
+
+//Opject
+
+$mobil = new Mobil();
+$kendaraan = new Kedaraan();
+$mobil->merek = 'Avansa';
+$mobil->model = 'Inova';
+$mobil->tahun = '2023';
+$mobil->warna = 'putih';
+
+//panggil Metode 
+$ket = '
+Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi visual seperti font, tipografi, dan tata letak. Maksud penggunaan lipsum adalah agar pengamat tidak terlalu berkonsentrasi kepada arti harfiah per kalimat, melainkan lebih kepada elemen desain dari teks yang dipresentasi. ';
+
+
+// Panggil Metode
+
+$mobil->cek('matius', $ket);
+$kendaraan->merek = 'BWM';
+echo $kendaraan->merek;
+$kendaraan->cek('matius', '');
+
+// $mobil->stop();
